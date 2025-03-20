b9a7899e8b65ac37dc709fca43e00b5d3fcc92be
veena <167426204+02vee@users.noreply.github.com>
2025-03-20 10:56:30 +0530

    Update folderRoutes.js

--- routes/folderRoutes.js
+++ routes/folderRoutes.js
@@ -1,1 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const { createFolder, renameFolder, deleteFolder, getFolders } = require('../controllers/folderController');
+const { isAdmin } = require('../middleware/authMiddleware');
 
+router.get('/', getFolders);
+router.post('/', isAdmin, createFolder);
+router.put('/:id', isAdmin, renameFolder);
+router.delete('/:id', isAdmin, deleteFolder);
+
+module.exports = router;
