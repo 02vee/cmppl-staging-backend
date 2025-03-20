4521a27932d3a270d3119c9e8db8582cc6b0e525
veena <167426204+02vee@users.noreply.github.com>
2025-03-20 16:03:22 +0530

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,1 +1,9 @@
-ECHO is on.
+const isAdmin = (req, res, next) => {
+  if (req.session && req.session.isAuthenticated) {
+    return next();
+  } else {
+    return res.status(401).json({ message: 'Unauthorized' });
+  }
+};
+
+module.exports = { isAdmin };
