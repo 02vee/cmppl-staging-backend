2bff7231cc7fd47263f932b487e4ad3f3cefbc66
veena <167426204+02vee@users.noreply.github.com>
2025-03-20 14:46:54 +0530

    Update certificates.js

--- vehicle-tracking-frontend/certificates.js
+++ vehicle-tracking-frontend/certificates.js
@@ -1,47 +1,53 @@
 const API_URL = 'https://cmppl-staging-backend.onrender.com';
+
 document.addEventListener('DOMContentLoaded', async () => {
-    const checkSession = async () => {
-      try {
-        const response = await fetch(`${API_URL}/api/auth/check-session`, {
-          method: 'GET',
-          credentials: 'include',
-        });
-  
-        const data = await response.json();
-        console.log('Session check data:', data);
-        if (!data.isAuthenticated) {
-          window.location.href = '/login.html';
-        } else {
-          fetchCertificates();
-        }
-      } catch (error) {
-        console.error('Session check failed:', error);
-        window.location.href = '/login.html';
-      }
-    };
-  
-    const fetchCertificates = async () => {
-      try {
-        const response = await fetch(`${API_URL}/api/certificate`, {
-          method: 'GET',
-          credentials: 'include',
-        });
-  
-        const certificates = await response.json();
-        const certificatesList = document.getElementById('certificates-list');
-        certificates.forEach(cert => {
-          const listItem = document.createElement('li');
-          const link = document.createElement('a');
-          link.href = cert.url;
-          link.textContent = cert.name;
-          link.target = '_blank';
-          listItem.appendChild(link);
-          certificatesList.appendChild(listItem);
-        });
-      } catch (error) {
-        console.error('Error fetching certificates:', error);
-      }
-    };
-  
-    await checkSession();
-  });
+  checkSession();
+  fetchCertificates();
+});
+
+const checkSession = async () => {
+  try {
+    const response = await fetch(`${API_URL}/api/auth/check-session`, {
+      method: 'GET',
+      credentials: 'include',
+    });
+
+    const data = await response.json();
+    console.log('Session check data:', data);
+    if (!data.isAuthenticated) {
+      window.location.href = '/login.html';
+    }
+  } catch (error) {
+    console.error('Session check failed:', error);
+    window.location.href = '/login.html';
+  }
+};
+
+const fetchCertificates = async () => {
+  try {
+    const response = await fetch(`${API_URL}/api/files`, {
+      method: 'GET',
+      credentials: 'include',
+    });
+
+    const certificates = await response.json();
+    const certificatesList = document.getElementById('certificates-list');
+
+    certificates.forEach(cert => {
+      const listItem = document.createElement('li');
+      const link = document.createElement('a');
+      link.href = `${API_URL}/api/files/${cert._id}/download`;
+      link.textContent = cert.name;
+      link.target = '_blank';
+      listItem.appendChild(link);
+      certificatesList.appendChild(listItem);
+    });
+  } catch (error) {
+    console.error('Error fetching certificates:', error);
+  }
+};
+
+const logout = () => {
+  fetch(`${API_URL}/api/auth/logout`, { method: 'GET', credentials: 'include' })
+    .then(response => window.location.href = '/login.html');
+};
