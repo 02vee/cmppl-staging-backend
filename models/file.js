e52aaf1d13a4f799b13b8a7769d3571dec3f99d7
veena <167426204+02vee@users.noreply.github.com>
2025-03-20 10:49:09 +0530

    Update file.js

--- models/file.js
+++ models/file.js
@@ -1,1 +1,10 @@
+const mongoose = require('mongoose');
 
+const fileSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  path: { type: String, required: true },
+  folder: { type: mongoose.Schema.Types.ObjectId, ref: 'Folder', required: true },
+  createdAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model('File', fileSchema);
